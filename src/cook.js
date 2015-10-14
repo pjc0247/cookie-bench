cdf5896f3cea7d8e15b3b702cfa9c391650280f1
Park June Chul <pjc0247@naver.com>
2015-10-15 02:03:16 +0900

    Create cook.js

---
+++ src/cook.js
@@ -0,0 +1,24 @@
+var getTicks = function(){
+	var d = new Date();
+    return d.getTime();
+}
+
+var startTicks = getTicks();
+var startClicks = Game.cookieClicks;
+
+var looper = setInterval(function(){ 
+	Game.ClickCookie();	
+	
+	var elapsed = getTicks() - startTicks;
+	if( elapsed >= 5000){
+
+		clearInterval(looper);
+
+		var total = Game.cookieClicks - startClicks;
+		var cps = total / (elapsed / 1000);
+
+		alert(
+			"Your brower have reached total " + total + "clicks in " + elapsed + "millisecond. \n" +
+			"CpS : " + Math.round(cps));
+	}
+}, 0);
